4c4e037ce7c57a0700e38042b65599400c2ba035
Shubham <svdixit8388@gmail.com>
2022-03-13 13:38:01 +0530

    add new inventory

--- src/admin/components/add-edit-inventory/add-edit-inventory.component.ts
+++ src/admin/components/add-edit-inventory/add-edit-inventory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-add-edit-inventory',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditInventoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
   }
 
+  goBack() {
+    this.location.back();
+  }
 }
